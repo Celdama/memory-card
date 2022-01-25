d0b42122cfbd806980fb8a39d9bd2bae2eb065b0
Celdama <celdama.dev@gmail.com>
2022-01-25 18:19:53 +0100

    components/ScoreBoard/scoreboard.style: refactor styles

--- src/components/ScoreBoard/scoreboard.style.js
+++ src/components/ScoreBoard/scoreboard.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  background-color: #000;
+  background-color: var(--black);
   padding: 30px 70px;
 `;
 
@@ -9,6 +9,6 @@ export const Content = styled.div`
   display: flex;
   justify-content: start;
   gap: 20px;
-  color: #fff;
+  color: var(--white);
   font-weight: bold;
 `;
