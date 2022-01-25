01bc2538187d53c8d2ec2d5dcb944f46fbeaa0a6
Celdama <celdama.dev@gmail.com>
2022-01-25 12:28:32 +0100

    components/Card: added new styled component in Card component

--- src/components/Card/card.style.js
+++ src/components/Card/card.style.js
@@ -1,34 +1,39 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.article`
+  border: 0.5px solid #fff;
+  border-radius: 5px;
+`;
+
+export const Content = styled.div`
   width: 340px;
   height: 340px;
   overflow: hidden;
   display: flex;
   flex-direction: column;
-  border: 0.5px solid #fff;
   border-radius: 5px;
   cursor: pointer;
 
-  div {
-    overflow: hidden;
-
-    img {
-      background-size: contain;
-      width: 100%;
-      height: auto;
-      transition: all ease 0.3s;
-    }
-  }
-
-  h5 {
-    color: #000;
-    text-align: center;
-    background-color: #fff;
-  }
-
   :hover img {
     transform: scale(1.03);
-    opacity: 0.8;
+    opacity: 1;
   }
+`;
+
+export const ImgContainer = styled.div`
+  overflow: hidden;
+`;
+
+export const Img = styled.img`
+  background-size: contain;
+  width: 100%;
+  height: auto;
+  transition: all ease 0.3s;
+  opacity: 0.8;
+`;
+
+export const Name = styled.h5`
+  color: #000;
+  text-align: center;
+  background-color: #fff;
 `;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Wrapper } from './card.style';
+import { Wrapper, ImgContainer, Content, Img, Name } from './card.style';
 
 const Card = ({ name, image, handleAlreadyClickedChars }) => {
   return (
-    // <Wrapper>
-    //   <Content onClick={handleAlreadyClickedChars} urlImg={image}>
-    //     <h5>{name}</h5>
-    //   </Content>
-    // </Wrapper>
-
-    <Wrapper>
-      <div className='img-container'>
-        <img src={image} alt='' />
-      </div>
-      <h5>{name}</h5>
+    <Wrapper onClick={handleAlreadyClickedChars}>
+      <Content>
+        <ImgContainer>
+          <Img src={image} alt={name} />
+        </ImgContainer>
+        <Name>{name}</Name>
+      </Content>
     </Wrapper>
   );
 };
